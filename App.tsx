5c5cb00bac63411236bc96c89ebca38e55803824
Alex Matos <alex.matos.freitas@gmail.com>
2021-06-24 20:43:11 -0300

    Refactored auth hook and updated route file name

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import AppLoading from 'expo-app-loading';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 
-LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine.']);
+// LogBox.ignoreLogs(['You are not currently signed in to Expo on your development machine.']);
 
 import { AuthProvider } from './src/hooks/auth';
 
