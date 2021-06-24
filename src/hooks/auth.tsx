a4c18b3476348fd2f69e809e8dd5872554b2d985
Alex Matos <alex.matos.freitas@gmail.com>
2021-06-24 20:55:24 -0300

    Added environment file support

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -10,13 +10,11 @@ import { api } from '../services/api';
 
 import * as AuthSession from 'expo-auth-session';
 
-import {
-	REDIRECT_URI,
-	SCOPE,
-	RESPONSE_TYPE,
-	CLIENT_ID,
-	CDN_IMAGE,
-} from '../configs';
+const { REDIRECT_URI } = process.env;
+const { SCOPE } = process.env;
+const { RESPONSE_TYPE } = process.env;
+const { CLIENT_ID } = process.env;
+const { CDN_IMAGE } = process.env;
 
 type User = {
 	id: string;
