202d86db679224aed938bc083de32c1c214b492f
Alex Matos <alex.matos.freitas@gmail.com>
2021-06-24 21:15:30 -0300

    Added local storage support and improve sign in

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -3,12 +3,15 @@ import React,
 	createContext,
 	useContext,
 	useState,
-	ReactNode
+	ReactNode,
+	useEffect,
 } from 'react';
 
 import { api } from '../services/api';
 
 import * as AuthSession from 'expo-auth-session';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { COLLECTION_USERS } from '../configs/storage';
 
 const { REDIRECT_URI } = process.env;
 const { SCOPE } = process.env;
@@ -64,12 +67,15 @@ function AuthProvider({ children }: AuthProviderProps) {
 
 				const firstName = userInfo.data.username.split(' ')[0];
 				userInfo.data.avatar = `${CDN_IMAGE}/avatars/${userInfo.data.id}/${userInfo.data.avatar}.png`;
-				
-				setUser({
+
+				const userData = {
 					...userInfo.data,
 					firstName,
 					token: params.access_token,
-				});
+				}
+				
+				await AsyncStorage.setItem(COLLECTION_USERS, JSON.stringify(userData));
+				setUser(userData);
 			} 
 		} catch {
 			throw new Error('Não foi possível autenticar');
@@ -77,6 +83,21 @@ function AuthProvider({ children }: AuthProviderProps) {
 			setLoading(false);
 		}
 	}
+
+	async function loadUserStorageData() {
+		const storage = await AsyncStorage.getItem(COLLECTION_USERS);
+
+		if (storage) {
+			const userLogged = JSON.parse(storage) as User; 
+			api.defaults.headers.authorization = `Bearer ${userLogged.token}`;
+
+			setUser(userLogged);
+		}
+	}
+
+	useEffect(() => {
+		loadUserStorageData();
+	}, []);
 	
 	return (
 		<AuthContext.Provider value={{ 
